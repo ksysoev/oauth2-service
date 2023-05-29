4620a6f5322a98d85a0d60b541b956087a686d88
Кирилл Сысоев <k.sysoev@me.com>
2023-05-29 22:08:35 +0800

    Adds mongodb

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 )
@@ -14,7 +17,28 @@ var (
 	oauthStateString  = "random"
 )
 
+var mongoClient *mongo.Client
+
 func main() {
+	// Set client options
+	clientOptions := options.Client().ApplyURI("mongodb://root:example@localhost:27017")
+
+	var err error
+	// Connect to MongoDB
+	mongoClient, err = mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		fmt.Println("Failed to connect to MongoDB:", err)
+		return
+	}
+
+	// Check the connection
+	err = mongoClient.Ping(context.Background(), nil)
+	if err != nil {
+		fmt.Println("Failed to ping MongoDB:", err)
+		return
+	}
+
+	fmt.Println("Connected to MongoDB!")
 	// Initialize the Google OAuth2 configuration
 	googleOauthConfig = &oauth2.Config{
 		ClientID:     "YOUR_CLIENT_ID",
@@ -37,6 +61,7 @@ func main() {
 	router.GET("/", handleHome)
 	router.GET("/auth/google/login", handleGoogleLogin)
 	router.GET("/auth/google/callback", handleGoogleCallback)
+	router.POST("/registration", handleProcessRegistration)
 
 	// Start the server
 	router.Run(":8080")
@@ -45,6 +70,42 @@ func main() {
 // handleHome is the handler for the home page.
 func handleHome(c *gin.Context) {
 	c.HTML(http.StatusOK, "home.html", gin.H{})
+}
+
+func handleRegistration(c *gin.Context) {
+	c.HTML(http.StatusOK, "registration.html", gin.H{})
+}
+
+type User struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func handleProcessRegistration(c *gin.Context) {
+	var user User
+	if err := c.BindJSON(&user); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err := mongoClient.Ping(context.Background(), nil)
+	if err != nil {
+		fmt.Println("Failed to ping MongoDB:", err)
+		return
+	}
+
+	res, err := mongoClient.Database("oauth2").Collection("users").InsertOne(context.Background(), user)
+
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	fmt.Println(res)
+
+	c.JSON(http.StatusOK, gin.H{"message": "User created successfully"})
+
 }
 
 // handleGoogleLogin is the handler for the Google OAuth2 login page.
